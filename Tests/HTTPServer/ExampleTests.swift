3e30b30a6d5bba07bab5cac06cebb0b022b85bd5
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-24 10:25:19 -0300

    fix indentation

--- Tests/HTTPServer/ExampleTests.swift
+++ Tests/HTTPServer/ExampleTests.swift
@@ -2,9 +2,9 @@ import XCTest
 @testable import HTTPServer
 
 class ExampleTests: XCTestCase {
-	func testReality() {
-		XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
-	}
+    func testReality() {
+        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+    }
 }
 
 extension ExampleTests {
