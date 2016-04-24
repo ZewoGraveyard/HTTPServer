21a6dca07e4b42b034d9430873e0449d91b5ea3c
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-24 10:31:16 -0300

    do math

--- Tests/HTTPServer/ExampleTests.swift
+++ Tests/HTTPServer/ExampleTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class ExampleTests: XCTestCase {
     func testReality() {
-        XCTAssert(2 + 2 == 4, "Something is severely wrong here.")
+        XCTAssert(1 + 2 == 3, "Something is severely wrong here.")
     }
 }
 
