4cfe9f31bd6308be3ce031823bdf13616734ac25
antonmes <antonmes@users.noreply.github.com>
2016-05-13 00:06:17 +0300

    Fix warnings

--- Source/Server.swift
+++ Source/Server.swift
@@ -67,7 +67,7 @@ extension Server {
     }
 
     // Own methods
-    public func start(_ failure: ErrorProtocol -> Void = Server.printError) throws {
+    public func start(_ failure: (ErrorProtocol) -> Void = Server.printError) throws {
         printHeader()
         while true {
             let stream = try server.accept(timingOut: .never)
@@ -112,7 +112,7 @@ extension Server {
         }
     }
 
-    public func startInBackground(_ failure: ErrorProtocol -> Void = Server.printError) {
+    public func startInBackground(_ failure: (ErrorProtocol) -> Void = Server.printError) {
         co {
             do {
                 try self.start()
