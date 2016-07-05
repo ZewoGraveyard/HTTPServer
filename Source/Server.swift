103ec7ac2b35fdea91cc14706d138351a8702a75
Paulo Faria <paulo.faria.rl@gmail.com>
2016-07-05 16:37:19 -0300

    prepare for quark

--- Source/Server.swift
+++ Source/Server.swift
@@ -28,15 +28,14 @@
 
 public struct Server {
     public let server: C7.Host
-    public let parser: S4.RequestParser
+    public let parser: S4.RequestParser.Type
     public let middleware: [S4.Middleware]
     public let responder: S4.Responder
-    public let serializer: S4.ResponseSerializer
+    public let serializer: S4.ResponseSerializer.Type
     public let port: Int
     public let bufferSize: Int = 2048
 
-    // Own initializers
-    public init(host: String = "0.0.0.0", port: Int = 8080, reusePort: Bool = false, parser: S4.RequestParser = RequestParser(), middleware: Middleware..., responder: Responder, serializer: S4.ResponseSerializer = ResponseSerializer()) throws {
+    public init(host: String = "0.0.0.0", port: Int = 8080, reusePort: Bool = false, parser: S4.RequestParser.Type = RequestParser.self, middleware: Middleware..., responder: Responder, serializer: S4.ResponseSerializer.Type = ResponseSerializer.self) throws {
         self.server = try TCPServer(host: host, port: port, reusePort: reusePort)
         self.parser = parser
         self.middleware = middleware
@@ -49,7 +48,11 @@ public struct Server {
         try self.init(responder: responder)
     }
 
-    public init(host: String = "0.0.0.0", port: Int = 8080, reusePort: Bool = false, parser: S4.RequestParser = RequestParser(), middleware: Middleware..., serializer: S4.ResponseSerializer = ResponseSerializer(), _ respond: Respond) throws {
+    public init(_ representable: ResponderRepresentable) throws {
+        try self.init(responder: representable.responder)
+    }
+
+    public init(host: String = "0.0.0.0", port: Int = 8080, reusePort: Bool = false, parser: S4.RequestParser.Type = RequestParser.self, middleware: Middleware..., serializer: S4.ResponseSerializer.Type = ResponseSerializer.self, _ respond: Respond) throws {
         self.server = try TCPServer(host: host, port: port, reusePort: reusePort)
         self.parser = parser
         self.middleware = middleware
@@ -60,20 +63,17 @@ public struct Server {
 }
 
 extension Server {
-
-    // S4.Server conformance
     public func start() throws {
-        try self.start(Server.printError)
+        try self.start(Server.log)
     }
 
-    // Own methods
-    public func start(_ failure: (ErrorProtocol) -> Void = Server.printError) throws {
+    public func start(_ failure: (ErrorProtocol) -> Void = Server.log) throws {
         printHeader()
         while true {
             let stream = try server.accept(timingOut: .never)
             co {
                 do {
-                    try self.processStream(stream)
+                    try self.process(stream: stream)
                 } catch {
                     failure(error)
                 }
@@ -81,40 +81,40 @@ extension Server {
         }
     }
 
-    private func processStream(_ stream: Stream) throws {
+    private func process(stream: Stream) throws {
+        let parser = self.parser.init(stream: stream)
+        let serializer = self.serializer.init(stream: stream)
+
         while !stream.closed {
             do {
-                let data = try stream.receive(upTo: bufferSize)
-                try processData(data, stream: stream)
-            } catch is SystemError {
-                break
-            } catch is StreamError {
-                break
+                let request = try parser.parse()
+                let response = try middleware.chain(to: responder).respond(to: request)
+                try serializer.serialize(response)
+
+                if let upgrade = response.didUpgrade {
+                    try upgrade(request, stream)
+                    try stream.close()
+                }
+
+                if !request.isKeepAlive {
+                    try stream.close()
+                }
             } catch {
-                let response = Response(status: .internalServerError)
-                try serializer.serialize(response, to: stream)
-                throw error
+                if stream.closed {
+                    break
+                }
+                if let response = Server.recover(error: error) {
+                    try serializer.serialize(response)
+                } else {
+                    let response = Response(status: .internalServerError)
+                    try serializer.serialize(response)
+                    throw error
+                }
             }
         }
     }
 
-    private func processData(_ data: Data, stream: Stream) throws {
-        if let request = try parser.parse(data) {
-            let response = try middleware.chain(to: responder).respond(to: request)
-            try serializer.serialize(response, to: stream)
-
-            if let upgrade = response.didUpgrade {
-                try upgrade(request, stream)
-                try stream.close()
-            }
-
-            if !request.isKeepAlive {
-                try stream.close()
-            }
-        }
-    }
-
-    public func startInBackground(_ failure: (ErrorProtocol) -> Void = Server.printError) {
+    public func startInBackground(_ failure: (ErrorProtocol) -> Void = Server.log) {
         co {
             do {
                 try self.start()
@@ -124,7 +124,92 @@ extension Server {
         }
     }
 
-    private static func printError(_ error: ErrorProtocol) -> Void {
+    private static func recover(error: ErrorProtocol) -> Response? {
+        switch error {
+        case ClientError.badRequest:
+            return Response(status: .badRequest)
+        case ClientError.unauthorized:
+            return Response(status: .unauthorized)
+        case ClientError.paymentRequired:
+            return Response(status: .paymentRequired)
+        case ClientError.forbidden:
+            return Response(status: .forbidden)
+        case ClientError.notFound:
+            return Response(status: .notFound)
+        case ClientError.methodNotAllowed:
+            return Response(status: .methodNotAllowed)
+        case ClientError.notAcceptable:
+            return Response(status: .notAcceptable)
+        case ClientError.proxyAuthenticationRequired:
+            return Response(status: .proxyAuthenticationRequired)
+        case ClientError.requestTimeout:
+            return Response(status: .requestTimeout)
+        case ClientError.conflict:
+            return Response(status: .conflict)
+        case ClientError.gone:
+            return Response(status: .gone)
+        case ClientError.lengthRequired:
+            return Response(status: .lengthRequired)
+        case ClientError.preconditionFailed:
+            return Response(status: .preconditionFailed)
+        case ClientError.requestEntityTooLarge:
+            return Response(status: .requestEntityTooLarge)
+        case ClientError.requestURITooLong:
+            return Response(status: .requestURITooLong)
+        case ClientError.unsupportedMediaType:
+            return Response(status: .unsupportedMediaType)
+        case ClientError.requestedRangeNotSatisfiable:
+            return Response(status: .requestedRangeNotSatisfiable)
+        case ClientError.expectationFailed:
+            return Response(status: .expectationFailed)
+        case ClientError.imATeapot:
+            return Response(status: .imATeapot)
+        case ClientError.authenticationTimeout:
+            return Response(status: .authenticationTimeout)
+        case ClientError.enhanceYourCalm:
+            return Response(status: .enhanceYourCalm)
+        case ClientError.unprocessableEntity:
+            return Response(status: .unprocessableEntity)
+        case ClientError.locked:
+            return Response(status: .locked)
+        case ClientError.failedDependency:
+            return Response(status: .failedDependency)
+        case ClientError.preconditionRequired:
+            return Response(status: .preconditionRequired)
+        case ClientError.tooManyRequests:
+            return Response(status: .tooManyRequests)
+        case ClientError.requestHeaderFieldsTooLarge:
+            return Response(status: .requestHeaderFieldsTooLarge)
+
+        case ServerError.internalServerError:
+            return Response(status: .internalServerError)
+        case ServerError.notImplemented:
+            return Response(status: .notImplemented)
+        case ServerError.badGateway:
+            return Response(status: .badGateway)
+        case ServerError.serviceUnavailable:
+            return Response(status: .serviceUnavailable)
+        case ServerError.gatewayTimeout:
+            return Response(status: .gatewayTimeout)
+        case ServerError.httpVersionNotSupported:
+            return Response(status: .httpVersionNotSupported)
+        case ServerError.variantAlsoNegotiates:
+            return Response(status: .variantAlsoNegotiates)
+        case ServerError.insufficientStorage:
+            return Response(status: .insufficientStorage)
+        case ServerError.loopDetected:
+            return Response(status: .loopDetected)
+        case ServerError.notExtended:
+            return Response(status: .notExtended)
+        case ServerError.networkAuthenticationRequired:
+            return Response(status: .networkAuthenticationRequired)
+
+        default:
+            return nil
+        }
+    }
+
+    private static func log(error: ErrorProtocol) -> Void {
         print("Error: \(error)")
     }
 
@@ -146,9 +231,9 @@ extension Server {
 }
 
 extension Request {
-    var connection: Header {
+    var connection: String? {
         get {
-            return headers.headers["connection"] ?? Header([])
+            return headers.headers["connection"]
         }
 
         set(connection) {
@@ -158,9 +243,9 @@ extension Request {
 
     var isKeepAlive: Bool {
         if version.minor == 0 {
-            return connection.values.contains({$0.lowercased().contains("keep-alive")})
+            return connection?.lowercased() == "keep-alive"
         }
-        return !connection.values.contains({$0.lowercased().contains("close")})
+        return connection?.lowercased() != "close"
     }
 }
 
@@ -176,5 +261,19 @@ extension Response {
         set(didUpgrade) {
             storage["response-connection-upgrade"] = didUpgrade
         }
+    }
+}
+
+extension Response {
+    init(status: Status = .ok, headers: Headers = [:], body: Data = []) {
+        self.init(
+            version: Version(major: 1, minor: 1),
+            status: status,
+            headers: headers,
+            cookieHeaders: [],
+            body: .buffer(body)
+        )
+
+        self.headers["Content-Length"] = body.count.description
     }
 }
