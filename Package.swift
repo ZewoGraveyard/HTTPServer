Zewo/Zewo

0.14 release (#177)

Package.swift
@@ -21,10 +21,10 @@ let package = Package(
         Target(name: "ExampleApplication", dependencies: ["HTTPServer"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/COpenSSL", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/CPOSIX.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/CYAJL.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/COpenSSL", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/CPOSIX.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/CYAJL.git", majorVersion: 0, minor: 14),
     ]
 )

ZewoGraveyard/HTTPServer

Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "HTTPServer",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/HTTPFile.git", majorVersion: 0, minor: 13),
-        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 13),
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/HTTPFile.git", majorVersion: 0, minor: 14),
+        .Package(url: "https://github.com/Zewo/TCP.git", majorVersion: 0, minor: 14),
     ]
 )